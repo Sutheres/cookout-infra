d86c522a63b868f173662e13265bbea890b52c2b
Ed <sutheres1963@gmail.com>
2020-08-16 09:03:29 -0700

    test

--- locals.tf
+++ locals.tf
@@ -1,75 +1,75 @@
 locals {
   subnets = {
     public-2a = {
-      vpc_id                  = aws_vpc.vpc.id
+      vpc_id                  = aws_vpc.mainvpc.id
       cidr_block              = "172.31.128.0/20"
       map_public_ip_on_launch = true
       availability_zone       = "us-west-2a"
     }
     private-a-2a = {
-      vpc_id                  = aws_vpc.vpc.id
+      vpc_id                  = aws_vpc.mainvpc.id
       cidr_block              = "172.31.0.0/19"
       map_public_ip_on_launch = false
       availability_zone       = "us-west-2a"
     }
     private-b-2a = {
-      vpc_id                  = aws_vpc.vpc.id
+      vpc_id                  = aws_vpc.mainvpc.id
       cidr_block              = "172.31.192.0/21"
       map_public_ip_on_launch = false
       availability_zone       = "us-west-2a"
     }
     spare-2a = {
-      vpc_id                  = aws_vpc.vpc.id
+      vpc_id                  = aws_vpc.mainvpc.id
       cidr_block              = "172.31.224.0/21"
       map_public_ip_on_launch = false
       availability_zone       = "us-west-2a"
     }
 
     public-2b = {
-      vpc_id                  = aws_vpc.vpc.id
+      vpc_id                  = aws_vpc.mainvpc.id
       cidr_block              = "172.31.144.0/20"
       map_public_ip_on_launch = true
       availability_zone       = "us-west-2b"
     }
     private-a-2b = {
-      vpc_id                  = aws_vpc.vpc.id
+      vpc_id                  = aws_vpc.mainvpc.id
       cidr_block              = "172.31.32.0/19"
       map_public_ip_on_launch = false
       availability_zone       = "us-west-2b"
     }
     private-b-2b = {
-      vpc_id                  = aws_vpc.vpc.id
+      vpc_id                  = aws_vpc.mainvpc.id
       cidr_block              = "172.31.200.0/21"
       map_public_ip_on_launch = false
       availability_zone       = "us-west-2b"
     }
     spare-2b = {
-      vpc_id                  = aws_vpc.vpc.id
+      vpc_id                  = aws_vpc.mainvpc.id
       cidr_block              = "172.31.232.0/21"
       map_public_ip_on_launch = false
       availability_zone       = "us-west-2b"
     }
 
     public-2c = {
-      vpc_id                  = aws_vpc.vpc.id
+      vpc_id                  = aws_vpc.mainvpc.id
       cidr_block              = "172.31.160.0/20"
       map_public_ip_on_launch = true
       availability_zone       = "us-west-2c"
     }
     private-a-2c = {
-      vpc_id                  = aws_vpc.vpc.id
+      vpc_id                  = aws_vpc.mainvpc.id
       cidr_block              = "172.31.64.0/19"
       map_public_ip_on_launch = false
       availability_zone       = "us-west-2c"
     }
     private-b-2c = {
-      vpc_id                  = aws_vpc.vpc.id
+      vpc_id                  = aws_vpc.mainvpc.id
       cidr_block              = "172.31.208.0/21"
       map_public_ip_on_launch = false
       availability_zone       = "us-west-2c"
     }
     spare-2c = {
-      vpc_id                  = aws_vpc.vpc.id
+      vpc_id                  = aws_vpc.mainvpc.id
       cidr_block              = "172.31.240.0/21"
       map_public_ip_on_launch = false
       availability_zone       = "us-west-2c"
--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,30 @@
-resource "aws_vpc" "vpc" {
+resource "aws_vpc" "mainvpc" {
   cidr_block = "172.31.0.0/16"
   instance_tenancy = "default"
   tags = {
     Name = "${terraform.workspace}-vpc"
+  }
+}
+
+resource "aws_default_network_acl" "default" {
+  default_network_acl_id = aws_vpc.mainvpc.default_network_acl_id
+
+  ingress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = aws_vpc.mainvpc.cidr_block
+    from_port  = 0
+    to_port    = 0
+  }
+
+  egress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
   }
 }
 
@@ -18,13 +40,13 @@ resource "aws_subnet" "subnets" {
 }
 
 resource "aws_route" "route" {
-  route_table_id = aws_vpc.vpc.main_route_table_id
+  route_table_id = aws_vpc.mainvpc.main_route_table_id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.gateway.id
 }
 
 resource "aws_internet_gateway" "gateway" {
-  vpc_id = aws_vpc.vpc.id
+  vpc_id = aws_vpc.mainvpc.id
   tags = {
     Name = "${terraform.workspace}-gateway"
   }
